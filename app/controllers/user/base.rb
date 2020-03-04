c388df64e882d0d5eee978d55fbdd468332795d1
tyamap <tyamap@tyamap.local>
2020-03-04 22:57:54 +0900

    [add] ユーザのsessionの基礎を作成

--- app/controllers/user/base.rb
+++ app/controllers/user/base.rb
@@ -1,3 +1,13 @@
 class User::Base < ApplicationController
-  
+
+  private
+
+  # ユーザログインのセッション情報
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :user do
     root 'top#index'
+    get "login" => "sessions#new", as: :login
+    resource :session, only: [ :create, :destroy ]
   end
 end

3ad2078c174bee989cb79f23dffec412d4777a43
tyamap <tyamap@tyamap.local>
2020-03-05 00:09:24 +0900

    [add] セッションによる認証機能を実装

--- app/controllers/user/base.rb
+++ app/controllers/user/base.rb
@@ -1,4 +1,5 @@
 class User::Base < ApplicationController
+  before_action :authorize
 
   private
 
@@ -10,4 +11,26 @@ class User::Base < ApplicationController
   end
 
   helper_method :current_user
+
+  # セッションによるフィルタリング
+  def authorize
+    unless current_user
+      flash.alert = "ログインしてください。"
+      redirect_to :user_login
+    end
+  end
+
+  TIMEOUT = 60.minutes
+  # セッションタイムアウトの設定
+  def check_timeout
+    if current_user
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:user_id)
+        flash.alert = "セッションがタイムアウトしました。"
+        redirect_to :user_login
+      end
+    end
+  end
 end

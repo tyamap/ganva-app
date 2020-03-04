b3ecc7918fac86e333f03452bea855d8f95fcabf
tyamap <tyamap@tyamap.local>
2020-03-05 00:33:49 +0900

    [mod] デファクタリング

--- app/models/concerns/password_holder.rb
+++ app/models/concerns/password_holder.rb
@@ -3,7 +3,7 @@ module PasswordHolder
 
   # パスワードをハッシュ化する
   def password=(raw_password)
-    if raw_password.kind_of?(String)
+    if raw_password.is_a?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
     elsif raw_password.nil?
       self.hashed_password = nil

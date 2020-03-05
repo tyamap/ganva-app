7da7fb0b2573757b21e269275f5ac54b051864f7
tyamap <tyamap@tyamap.local>
2020-03-05 22:03:23 +0900

    [add] ユーザーのプロフィールを表示
    user/{uid}で任意のユーザーのプロフィールを表示する

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :user do
     root 'top#index'
-    get "login" => "sessions#new", as: :login
+    get 'login' => 'sessions#new', as: :login
+    get '/:uid' => 'accounts#show'
     resource :session, only: [ :create, :destroy ]
   end
 end

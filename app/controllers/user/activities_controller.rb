4d412f5f91901ae7265c5100af8c76e486b2e62f
tyamap <tyamap@tyamap.local>
2020-03-07 19:13:54 +0900

    [add] アクティビティ表示の土台を作成

---
+++ app/controllers/user/activities_controller.rb
@@ -0,0 +1,22 @@
+class User::ActivitiesController < User::Base
+
+  def index
+    if params[:uid]
+      uid = params[:uid]
+      @show_user = User.find_by(uid: uid)
+      @activities = @show_user.name + 'さんのアクティビティ'
+    else
+      @activities = 'あなたのアクティビティ'
+    end
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+end

30eb8324fa8ad2fb3f4414096ca039c6a532bf5a
tyamap <tyamap@tyamap.local>
2020-03-07 23:03:15 +0900

    [mod] テーブルの調節

--- db/migrate/20200304130139_create_users.rb
+++ db/migrate/20200304130139_create_users.rb
@@ -10,6 +10,15 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
+    create_table :profiles do |t|
+      t.belongs_to :user, null: false, index: { unique: true }, foreign_key: true
+      t.string :experience, null: false    # 経験 
+      t.string :frequency, null: false     # ボルダリング頻度
+      t.string :level, null: false         # レベル
+      t.string :status, null: false        # ステータス
+
+      t.timestamps
+    end
     add_index :users, 'LOWER(email)', unique: true
     add_index :users, 'LOWER(uid)', unique: true
   end

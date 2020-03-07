9a6c14df2624f9d736782ebbbc640939794e67b3
tyamap <tyamap@tyamap.local>
2020-03-07 14:58:18 +0900

    [mod] ルーティングの改善

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
     get 'login' => 'sessions#new', as: :login
     resource :session, only: [ :create, :destroy ]
 
+    resources :accounts, only: [ :create ,:index ]
+    get '/register' => 'accounts#new', as: :register
+    resource :account, except: [ :new, :create, :delete ]
     get '/:uid' => 'accounts#show'
-    resources :accounts, except: [ :index, :show ]
 
     get '/home' => 'accounts#home', as: :home
   end

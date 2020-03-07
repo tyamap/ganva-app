932ebfb2cbdd84562ac2e8bc82a691b6edbb1816
tyamap <tyamap@tyamap.local>
2020-03-07 20:21:31 +0900

    [mod] リファクタリング

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,14 +1,12 @@
 class User::UsersController < User::Base
-  skip_before_action :authorize, only: [:new, :create]
+  skip_before_action :authorize, only: %i[new create]
 
   def show
     uid = params[:uid]
     @show_user = User.find_by(uid: uid)
   end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 end

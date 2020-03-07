2fffbccef8b4957748b338dcf12e5b343af94e7e
tyamap <tyamap@tyamap.local>
2020-03-08 01:27:50 +0900

    [mod] ルーティングの見直し
    シードの微修正

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -1,1 +1,2 @@
-User.create!(email: 'test@example.com', password: 'password', name: 'テスト', uid: 'test_admin', experience: '5', frequency: '1',level: '3', status: 'stable', introduction: 'どーも。') 
+User.create!(email: 'test@example.com', password: 'password', name: 'テスト', uid: 'test_admin', 
+  experience: '5', frequency: '1',level: '3', status: 'stable', introduction: 'どーも。') 

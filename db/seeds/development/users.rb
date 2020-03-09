61b2c4e53340dd217973ef884ba2a3f1ed7982fa
tyamap <tyamap@tyamap.local>
2020-03-10 00:03:36 +0900

    [add] シードデータを作成

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -1,2 +1,44 @@
-User.create!(email: 'test@example.com', password: 'password', name: 'テスト', uid: 'test_admin', 
-  experience: '5', frequency: '1',level: '3', status: 'stable', introduction: 'どーも。') 
+User.create!(
+  email: 'test@example.com', 
+  password: 'password', 
+  name: 'テスト', 
+  uid: 'test_admin', 
+  experience: '5', 
+  frequency: '1',
+  level: '3', 
+  status: 'stable', 
+  introduction: 'どーも。'
+) 
+
+  
+  names_1 = %w{
+    佐藤:sato
+    鈴木:suzuki
+    高橋:takahashi
+    田中:tanaka
+  }
+  
+  names_2 = %w{
+    ジロウ:jiro
+    サブロウ:saburo
+    マツコ:matsuko
+    タケコ:takeko
+    ウメコ:umeko
+  }
+  
+  20.times do |n|
+    un1 = names_1[n % 4].split(":")
+    un2 = names_2[n % 5].split(":")
+  
+    User.create!(
+      email: "#{un1[1]}_#{un2[1]}@example.com",
+      password: "password",
+      name: un1[0] + un2[0],
+      uid: un1[1] + un2[1],
+      experience: n % 10, 
+      frequency: n % 5,
+      level: n % 6, 
+      status: 'stable', 
+      introduction: 'テスト用です。'
+    )
+  end

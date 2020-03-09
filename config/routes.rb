9a4d7c63835f4a76706fd5af1a9e62fb9f43fd5c
tyamap <tyamap@tyamap.local>
2020-03-09 21:32:28 +0900

    [mod] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   namespace :user, path: '' do
     root 'top#index'
-    resources :users, only: [ :new, :create ,:index ]
+    resources :users,           only: [ :new, :create ,:index ]
     get 'login' => 'sessions#new', as: :login
-    resource :session, only: [ :create, :destroy ]
+    resource :session,          only: [ :create, :destroy ]
     
     get '/home' => 'accounts#show', as: :home
-    resource :account, except: [ :new, :create, :delete ]
+    resource :account,          except: [ :new, :create, :delete, :destroy ]
     resources :activities
+    resources :achievements
+    resources :relationships,   only: [ :create, :destroy ]
+
     resources :users, param: :uid, path: '/', only: [ :show ]
     scope '/:uid' do
-      resources :activities, only: [ :index, :show ], as: :user_activities
+      resources :activities,    only: [ :index, :show ],            as: :user_activities
+      resources :achievements,  only: [ :index, :show ],            as: :user_achievements
+      resources :following,     only: [ :index ],                   as: :user_following
+      resources :followers,     only: [ :index ],                   as: :user_followers
     end
   end
 end

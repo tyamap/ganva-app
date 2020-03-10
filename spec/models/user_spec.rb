ebdec0120adfb540c2d211cfcae4909af9c2a420
tyamap <tyamap@tyamap.local>
2020-03-10 22:06:03 +0900

    [add] User_model_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#password=" do
+    it "文字列を与えると60文字の hashed_password を返す" do
+      user =  User.new
+      user.password = "password"
+      expect( user.hashed_password).to be_kind_of(String)
+      expect( user.hashed_password.size).to eq(60)
+    end
+
+    it "nilを与えるとnilになる" do
+      user =  User.new(hashed_password: "x")
+      user.password = nil
+      expect( user.hashed_password).to be_nil
+    end
+  end
 end

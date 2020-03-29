3f3d3dd22375f9ef25d48c3a9f3fd8264f47ed28
tyamap <tom333my@outlook.com>
2020-03-29 18:12:47 +0900

    [add] アクティビティ追加画面の作成

--- app/controllers/user/activities_controller.rb
+++ app/controllers/user/activities_controller.rb
@@ -12,12 +12,26 @@ class User::ActivitiesController < User::Base
   def show
     @activity   = Activity.find(params[:id])
     @commit     = @activity.commit_record
-    @commit_gym = Gym.find(@commit.gym_id)
+    @commit_gym = Gym.find(@commit.gym_id) unless @commit.nil?
     @result     = @activity.result_record
-    @result_gym = Gym.find(@result.gym_id)
+    @result_gym = Gym.find(@result.gym_id) unless @result.nil?
   end
 
-  def new; end
+  def new 
+    @activity = Activity.new(flash[:activity])
+  end
+
+  def create
+    @activity_form = User::ActivityForm.new(current_user)
+    @activity_form.assign_attributes(params)
+    if @activity_form.save!
+      flash.notice = "アクティビティを追加しました。"
+      redirect_to action: "index"
+    else
+      flash.alert = "入力に誤りがあります。"
+      render action: "new"
+    end
+  end
 
   def edit; end
 end

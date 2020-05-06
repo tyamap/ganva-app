41ea1bc5f71e2bb2cca7cf869907047929e6ed88
tyamap <tom333my@outlook.com>
2020-05-06 15:08:58 +0900

    [add] フォロー一覧表示機能

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -25,6 +25,20 @@ class User::UsersController < User::Base
     end
   end
 
+  def following
+    @title = 'Following'
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+
   private
 
   def user_params

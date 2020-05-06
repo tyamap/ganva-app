079118fe5fdeb6ce3a4935753462c0e8d94fdfee
tyamap <tom333my@outlook.com>
2020-05-06 14:36:59 +0900

    [mod] フォロー機能の修正

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -2,15 +2,12 @@ class User::RelationshipsController < User::Base
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    redirect_to [:user, user]
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to user
+    redirect_to [:user, user]
   end
 end

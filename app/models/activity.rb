435ffe53967d60509f109393660dbd336bb66534
tyamap <tom333my@outlook.com>
2020-05-07 21:51:46 +0900

    [add] 日時取得関数の実装

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,14 @@ class Activity < ApplicationRecord
   belongs_to :gym, optional: true
 
   has_one :level_count, dependent: :destroy, autosave: true
+
+  # 開始日時をTime型で返す
+  def start_datetime
+    Time.zone.parse("#{date} #{start_time}")
+  end
+
+  # 終了日時をTime型で返す
+  def end_datetime
+    Time.zone.parse("#{date} #{end_time}")
+  end
 end

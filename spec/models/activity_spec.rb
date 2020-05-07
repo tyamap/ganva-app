9cae6f8ec856bb253a88607092e4cd9ab71ddf83
tyamap <tom333my@outlook.com>
2020-05-07 23:12:54 +0900

    [mod] Specの修正 (RED)

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,47 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
+  # User 定義
+  let(:spidey) {build :user, :with_commit_activities}
+  let(:ironman) {build :user, :with_result_activities}
+  
+  # 宣言と結果
+  let(:commit_act) {spidey.activities.first}
+  let(:result_act) {ironman.activities.first}
+
+  # 現在日時
+  let(:dt_now) {Time.zone.parse("2020-4-1 9:30:00")}
+
+  # 日付
+  let(:today) {dt_now.strftime("%Y-%m-%d")}
+  let(:yesterday) {dt_now.yesterday.strftime("%Y-%m-%d")}
+  let(:tomorrow) {dt_now.tomorrow.strftime("%Y-%m-%d")}
+
+  # 時間
+  let(:nowtime) {dt_now.strftime("%H:%M:%S")} 
+  let(:hour_ago) {dt_now.ago(1.hours).strftime("%H:%M:%S")} 
+  let(:two_hour_ago) {dt_now.ago(2.hours).strftime("%H:%M:%S")} 
+  let(:hour_togo) {dt_now.since(1.hours).strftime("%H:%M:%S")} 
+  let(:two_hour_togo) {dt_now.since(2.hours).strftime("%H:%M:%S")} 
+
   describe 'validation' do
-    # User 定義
-    let(:spidey) {build :user, :with_commit_activities}
-    let(:ironman) {build :user, :with_result_activities}
-    
-    # 宣言と結果
-    let(:commit_act) {spidey.activities.first}
-    let(:result_act) {ironman.activities.first}
 
-    # 現在日時
-    let(:dt_now) {Time.current}
-
-    # 日付
-    let(:today) {dt_now.strftime("%Y-%m-%d")}
-    let(:yesterday) {dt_now.yesterday.strftime("%Y-%m-%d")}
-    let(:tomorrow) {dt_now.tomorrow.strftime("%Y-%m-%d")}
-
-    # 時間
-    let(:nowtime) {dt_now.strftime("%H:%M:%S")} 
-    let(:hour_ago) {dt_now.ago(1.hours).strftime("%H:%M:%S")} 
-    let(:two_hour_ago) {dt_now.ago(2.hours).strftime("%H:%M:%S")} 
-    let(:hour_togo) {dt_now.since(1.hours).strftime("%H:%M:%S")} 
-    let(:two_hour_togo) {dt_now.since(2.hours).strftime("%H:%M:%S")} 
-
-    example '開始時刻が終了時刻より前のものは登録できない' do
+    example '開始時刻が終了時刻より後のものは登録できない' do
       commit_act.date = today
       commit_act.start_time = two_hour_togo
       commit_act.end_time = hour_togo
+      expect(commit_act.start_datetime > commit_act.end_datetime).to be_truthy
       expect(commit_act).not_to be_valid
     end
     example '開始時刻が終了時刻と等しいものは登録できない' do
       commit_act.date = today
       commit_act.start_time = hour_togo
       commit_act.end_time = hour_togo
+      expect(commit_act.start_datetime == commit_act.end_datetime).to be_truthy
       expect(commit_act).not_to be_valid
     end
-    example '開始時刻が終了時刻より１秒前のものは登録できない' do
-      commit_act.date = yesterday
-      commit_act.start_time = '14:00:01'
-      commit_act.end_time = '14:00:00'
-      commit_act.status = Settings.activity.status_done
+    example '開始時刻が終了時刻より１秒後のものは登録できない' do
+      commit_act.date = today
+      commit_act.start_time = dt_now.since(60.seconds).strftime("%H:%M:%S")
+      commit_act.end_time = dt_now.since(59.seconds).strftime("%H:%M:%S")
+      expect(commit_act.start_datetime - commit_act.end_datetime).to eq 1.0
       expect(commit_act).not_to be_valid
     end
 
@@ -144,11 +147,13 @@ RSpec.describe Activity, type: :model do
         end
       end
     end
+  end
 
-    #
-    # ステータス変更
-    #
-    describe 'update status' do
+  #
+  # ステータス変更
+  #
+  describe 'update' do
+    describe 'status' do
       example 'ready から aborted に変更できる' do
         spidey.save!
         expect(spidey.activities.first.status).to eq Settings.activity.status_ready

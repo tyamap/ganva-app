b08ab43d2ea0a87967bd44aa2c3c6319d15235f4
tyamap <tom333my@outlook.com>
2020-05-09 03:02:15 +0900

    [mod] 循環的複雑度の妥協点

--- app/models/concerns/activity_status_holder.rb
+++ app/models/concerns/activity_status_holder.rb
@@ -20,17 +20,15 @@ module ActivityStatusHolder
 
   # statusの変更バリデーション：statusの整合性を保つ
   def able_to_change?
+    return if status_was == status
+
     case status_was
     when Settings.activity.status.recorded
-      errors.add(:status, 'cannot change from recorded') if status != Settings.activity.status.recorded
+      errors.add(:status, 'cannot change from recorded')
     when Settings.activity.status.aborted
-      if status == Settings.activity.status.done || status == Settings.activity.status.recorded
-        errors.add(:status, 'cannot change from aborted')
-      end
+      errors.add(:status, 'cannot change from aborted') if status != Settings.activity.status.ready
     when Settings.activity.status.done
-      if status == Settings.activity.status.ready || status == Settings.activity.status.aborted
-        errors.add(:status, 'cannot change from done')
-      end
+      errors.add(:status, 'cannot change from done') if status != Settings.activity.status.recorded
     end
   end
 end

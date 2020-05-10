9efa0cb42a1b28ca3273c03178033a01f84a07a7
tyamap <tom333my@outlook.com>
2020-05-11 00:31:04 +0900

    [mod] リファクタリング

--- app/controllers/user/activities_controller.rb
+++ app/controllers/user/activities_controller.rb
@@ -11,11 +11,11 @@ class User::ActivitiesController < User::Base
   def show
     @activity = Activity.find(params[:id])
     @gym = @activity.gym
-    if @activity.status == Settings.activity.status.recorded
-      # 結果レベル情報の取得
-      @lc_attr = @activity.level_count.attributes.values[2..11]
-      @ln_attr = @gym.level_name.attributes.values[2..11]
-    end
+    return unless @activity.status == Settings.activity.status.recorded
+
+    # 結果レベル情報の取得
+    @lc_attr = @activity.level_count.attributes.values[2..11]
+    @ln_attr = @gym.level_name.attributes.values[2..11]
   end
 
   def new_commit

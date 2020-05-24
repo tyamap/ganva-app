e2cb28bf8479c949f0f5760af1cceb7f580fb3ba
tyamap <tom333my@outlook.com>
2020-05-24 15:46:02 +0900

    [mod] fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,25 @@ Rails.application.routes.draw do
   namespace :user, path: '' do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
-    resource :session,          only: [ :create, :destroy ]
-    
+    resource :session, only: %i[create destroy]
+
     get 'home' => 'top#home', as: :home
-    resource :account, only: [ :show, :edit, :update ]
-    resource :profile, only: [ :show, :edit, :update ]
-    resource :password, only: [ :show, :edit, :update ]
-    resources :activities, except: [ :new ] do
-      collection do 
+    resource :account, only: %i[show edit update]
+    resource :profile, only: %i[show edit update]
+    resource :password, only: %i[show edit update]
+    resources :activities, except: %i[new] do
+      collection do
         get 'commit/new', action: 'new_commit'
         get 'result/new', action: 'new_result'
       end
     end
-    resources :achievements, only: [ :index, :show ]
-    resources :gyms, only: [ :index, :show ]
-    resources :relationships,   only: [ :create, :destroy ]
+    resources :achievements, only: %i[index show]
+    resources :gyms, only: %i[index show]
+    resources :relationships, only: %i[create destroy]
 
     resources :users do
-      resources :activities,    only: [ :index, :show ]
-      resources :achievements,  only: [ :index, :show ]
+      resources :activities,    only: %i[index show]
+      resources :achievements,  only: %i[index show]
       member do
         get :following, :followers
       end
